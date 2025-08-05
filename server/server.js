06230489f54add5a303b2859c8ab75007689e229
Buggy-Sama0 <157221907+Buggy-Sama0@users.noreply.github.com>
2025-08-05 22:24:39 +0800

    Hello World

--- server/server.js
+++ server/server.js
@@ -43,7 +43,8 @@ mongoose.connection.on('connected', () => {
 
 
 
-// middleware to check status
+// middleware to check status 
+/*
 app.use((req, res, next) => {
   if (mongoose.connection.readyState!==1) {
     return res.status(500).json({
@@ -52,7 +53,7 @@ app.use((req, res, next) => {
     });
   }
   next();
-})
+})*/
 
 // Set up Multer storage
 
