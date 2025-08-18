d8644354ce6d541024a77c49463f6ca3bd9a3593
Buggy-Sama0 <157221907+Buggy-Sama0@users.noreply.github.com>
2025-08-18 15:23:49 +0800

    Hello World

--- client/src/components/Requirement.jsx
+++ client/src/components/Requirement.jsx
@@ -31,7 +31,7 @@ function Requirements() {
   };
 
   const handleFileChange = e => {
-    setFiles([...files],e.target.file)
+    setFiles([...files],e.target.files)
     console.log(files); 
   };
 
